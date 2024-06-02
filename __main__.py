04611586cbf47f76a19d2b1d129060c0a6ed4aca
wangzhaohan2910 <wangcf3000@sina.com>
2024-06-02 20:31:27 +0800

    Stonger the SayHello function in __main__.py

--- __main__.py
+++ __main__.py
@@ -5,6 +5,10 @@ def SayHello():
     print("and want to use it in your project,")
     print("please obey the MIT License.")
     print("(But, I don't think it will be useful.)")
+    print("In the next part of this program,")
+    print("you need to type some commands.")
+    print("If you don't know the function of each command,")
+    print('then type "help".')
 
 
 if __name__ == "__main__":
