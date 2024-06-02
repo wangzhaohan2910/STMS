5fb1ff356ff7b5a38bf633fc48e9ccece5d36534
wangzhaohan2910 <wangcf3000@sina.com>
2024-06-02 20:24:00 +0800

    Today I wrote the SayHello in __main__.py

--- __main__.py
+++ __main__.py
@@ -1,3 +1,11 @@
-import csv
+def SayHello():
+    print("Welcome to STMS.")
+    print("This project uses MIT License.")
+    print("If you think this project is useful,")
+    print("and want to use it in your project,")
+    print("please obey the MIT License.")
+    print("(But, I don't think it will be useful.)")
 
-print(csv.list_dialects())
+
+if __name__ == "__main__":
+    SayHello()
