ba328b7265fc042cd16d5a0f2b476fd2513c530b
wangzhaohan2910 <wangcf3000@sina.com>
2024-06-07 13:53:14 +0800

    Strongen __main__.py

--- __main__.py
+++ __main__.py
@@ -1,9 +1,11 @@
 from cmd import Cmd
 from argparse import ArgumentParser
 
+gData = None
+
 
 class cApp(Cmd):
-    prompt = "Command(? for help)>"
+    prompt = "Command>"
 
     def do_EOF(self, line):
         """Exit the program."""
@@ -13,5 +15,8 @@ class cApp(Cmd):
 if __name__ == "__main__":
     hParser = ArgumentParser(description="The STMS.")
     hParser.add_argument("file", type=open)
+    tSplited = map(
+        lambda sLine: sLine.split(" "), hParser.parse_args().file.read().split("\n")
+    )
     print("Welcome to the STMS.")
     cApp().cmdloop()
