130a68a2a873953259a317b650ea583dff084adf
wangzhaohan2910 <wangcf3000@sina.com>
2024-06-08 10:13:21 +0800

    Today I finished parsing the content in the file that user entered in the command line option to the gData.

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 from cmd import Cmd
 from argparse import ArgumentParser
 
-gData = None
+gData = {}
 
 
 class cApp(Cmd):
@@ -15,8 +15,15 @@ class cApp(Cmd):
 if __name__ == "__main__":
     hParser = ArgumentParser(description="The STMS.")
     hParser.add_argument("file", type=open)
-    tSplited = map(
+    for lPair in map(
         lambda sLine: sLine.split(" "), hParser.parse_args().file.read().split("\n")
-    )
+    ):
+        if len(lPair) > 1:
+            sTeacherName = lPair[0]
+            sStudentName = lPair[1]
+            try:
+                gData[sTeacherName].add(sStudentName)
+            except:
+                gData[sTeacherName] = {sStudentName}
     print("Welcome to the STMS.")
     cApp().cmdloop()
