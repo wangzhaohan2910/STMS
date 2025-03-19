86ce9aee0ae4d7f3c7499203aaceb21c9e77dc68
wangzhaohan2910 <wangcf3000@sina.com>
2025-03-19 18:15:57 +0800

    DB Done!

--- __main__.py
+++ __main__.py
@@ -74,8 +74,68 @@ class Database:
     def getStudentXor(self, *lId):
         return {i.iId for i in self.TCH if sum(j in i.setStudent for j in lId) % 2 != 0}
 
+    def getStudentNot(self, iId):
+        return {i.iId for i in self.TCH if iId not in i.setStudent}
+
+    def setStudentNot(self, iId):
+        for i in self.TCH:
+            if iId in i.setStudent:
+                i.setStudent.discard(iId)
+            else:
+                i.setStudent.add(iId)
+
+    def getStudentAndTeacher(self, iId, *lTeacher):
+        return {
+            t.iId
+            for t in self.TCH
+            if (iId in t.setStudent) and all(stu in t.setStudent for stu in lTeacher)
+        }
+
+    def getStudentOrTeacher(self, iId, *lTeacher):
+        return {
+            t.iId
+            for t in self.TCH
+            if (iId in t.setStudent) or any(stu in t.setStudent for stu in lTeacher)
+        }
+
+    def getStudentXorTeacher(self, iId, *lTeacher):
+        return {
+            t.iId
+            for t in self.TCH
+            if (iId in t.setStudent)
+            ^ (sum(stu in t.setStudent for stu in lTeacher) % 2 != 0)
+        }
+
+    def setStudentAndTeacher(self, iId, *lTeacher):
+        for i in self.TCH:
+            if i.iId in lTeacher:
+                i.setStudent.add(iId)
+            else:
+                i.setStudent.discard(iId)
+
+    def getStudentOrTeacher(self, iId, *lTeacher):
+        return {i.iId for i in self.TCH if any(j in i.setStudent for j in lTeacher)}
+
+    def setStudentOrTeacher(self, iId, *lTeacher):
+        for i in self.TCH:
+            if i.iId in lTeacher:
+                i.setStudent.add(iId)
+
+    def getStudentXorTeacher(self, iId, *lTeacher):
+        return {
+            i.iId for i in self.TCH if sum(j in i.setStudent for j in lTeacher) % 2 != 0
+        }
+
+    def setStudentXorTeacher(self, iId, *lTeacher):
+        for i in self.TCH:
+            if i.iId in lTeacher:
+                if iId in i.setStudent:
+                    i.setStudent.discard(iId)
+                else:
+                    i.setStudent.add(iId)
+
     def getAllStudentName(self):
-        return self.STU
+        return {i for i in self.STU if i}
 
     def getAllStudentTeacher(self):
         dResult = {}
@@ -152,8 +212,62 @@ class Database:
                 setResult.add(j)
         return setResult
 
+    def getTeacherNot(self, iId):
+        return {
+            j for j in range(1, len(self.STU)) if iId not in self.getStudentTeacher(j)
+        }
+
+    def setTeacherNot(self, iId):
+        for j in range(1, len(self.STU)):
+            if j in self.TCH[iId].setStudent:
+                self.TCH[iId].setStudent.discard(j)
+            else:
+                self.TCH[iId].setStudent.add(j)
+
+    def getTeacherAndStudent(self, iId, *lStudent):
+        return {
+            j
+            for j in range(1, len(self.STU))
+            if (iId in self.getStudentTeacher(j))
+            and all(stu in self.getStudentTeacher(j) for stu in lStudent)
+        }
+
+    def getTeacherOrStudent(self, iId, *lStudent):
+        return {
+            j
+            for j in range(1, len(self.STU))
+            if (iId in self.getStudentTeacher(j))
+            or any(stu in self.getStudentTeacher(j) for stu in lStudent)
+        }
+
+    def getTeacherXorStudent(self, iId, *lStudent):
+        return {
+            j
+            for j in range(1, len(self.STU))
+            if (iId in self.getStudentTeacher(j))
+            ^ (sum(stu in self.getStudentTeacher(j) for stu in lStudent) % 2 != 0)
+        }
+
+    def setTeacherAndStudent(self, iId, *lStudent):
+        for j in range(1, len(self.STU)):
+            if j in lStudent:
+                self.TCH[iId].setStudent.add(j)
+            else:
+                self.TCH[iId].setStudent.discard(j)
+
+    def setTeacherOrStudent(self, iId, *lStudent):
+        for j in lStudent:
+            self.TCH[iId].setStudent.add(j)
+
+    def setTeacherXorStudent(self, iId, *lStudent):
+        for j in lStudent:
+            if j in self.TCH[iId].setStudent:
+                self.TCH[iId].setStudent.discard(j)
+            else:
+                self.TCH[iId].setStudent.add(j)
+
     def getAllTeacherName(self):
-        return {i.sName for i in self.TCH}
+        return {i.sName for i in self.TCH if i.sName}
 
     def getAllTeacherStudent(self):
         return {i.iId: i.setStudent for i in self.TCH}
